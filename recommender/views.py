59881a870d1aea38c8473705e02d1facc3968be0
Group 7 <latejah.whittaker.938@my.csun.edu>
2024-04-18 19:26:51 -0700

    more updates

--- recommender/views.py
+++ recommender/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'recommender/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'recommender/post_detail.html', {'post': post})
