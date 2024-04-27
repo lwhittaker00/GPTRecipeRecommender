8969b9d9829d7413573f0c55f9cadfb5d206bb4a
Group 7 <latejah.whittaker.938@my.csun.edu>
2024-04-26 18:31:27 -0700

    remove unused code

--- recommender/views.py
+++ recommender/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from .models import Recipes
 from .forms import PostForm
 from .utils import GetFoodRec
 
@@ -57,48 +56,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'recommender/post_edit.html', {'form': form})
-
-def CreateRecommendation(request):
-    MealRecommendation = None
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            # Extract data
-            title = form.cleaned_data['title']
-            ingredients = form.cleaned_data['ingredients']
-            tools = form.cleaned_data['tools']
-
-            MealRecommendation = GetFoodRec(title, ingredients, tools)
-
-            if MealRecommendation:                
-                # Save the recipe with recommendation
-                Recipes.objects.create(
-                    title = title,
-                    ingredients = ingredients,
-                    tools = tools,
-                    recommendation = MealRecommendation,
-                )
-                # Redirect to the recipes page
-                return redirect('my_recipes')
-            else:
-                # Log or display error message
-                print("Error: Recommendation could not be generated.")
-    else:
-        form = PostForm()
-
-    return render(request, 'recommender/create_recommendation.html', {'form': form})
-
-def recipes_page(request):
-    recipes = Recipes.objects.all()
-    return render(request, 'recommender/my_recipes.html', {'recipes': recipes})
-
-def delete_recipe(request, recipe_id):
-    recipe = get_object_or_404(Recipes, pk=recipe_id)
-    if request.method == 'POST':
-        recipe.delete()
-        return redirect('my_recipes')
-    else:
-        pass
 
 def delete_recommendation(request, post_id):
     recommendation = get_object_or_404(Post, pk=post_id)
